c9a0ca5184c8783b8ff7040fc60349a37ee8e85e
Mazhar saifi <84468625+twister904@users.noreply.github.com>
2023-09-17 23:49:33 +0530

    initial commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,58 @@
+var numberofbuttons=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberofbuttons;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttoninnerhtml=this.innerHTML;
+        makesound(buttoninnerhtml);
+        buttonAnimation(buttoninnerhtml);
+    });
+}
+document.addEventListener("keyup",function(event){
+    makesound(event.key);
+    buttonAnimation(event.key);
+
+});
+function makesound(key)
+{
+    switch (key)
+    {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+
+            break;
+        case "d":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+         break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+        break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+        break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+         break;
+
+    }
+}
+function buttonAnimation(currentkey)
+{
+    var activebutton=document.querySelector("."+currentkey);
+    activebutton.classList.add("pressed");
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    },300);
+}
